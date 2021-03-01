60199aff0ab80bf407676a5922c2e636b84c39f8
Zahida Hassan Pervaiz <69922404+zhp0006@users.noreply.github.com>
2021-02-28 20:51:06 -0600

    Tried to  edit function  following best practicing.

--- smallest_factor.py
+++ smallest_factor.py
@@ -4,19 +4,23 @@
 
 import sys
 
-if len(sys.argv) != 2:
-    sys.exit(sys.argv[0] + ": Expecting one command line argument -- the integer for which to get the smallest factor")
-n = int(sys.argv[1])
-if n < 1:
-    sys.exit(sys.argv[0] + ": Expecting a positive integer")
+def smallest_prime_factor(n):
+    if len(sys.argv) != 2:
+        sys.exit(sys.argv[0] + "Expecting one command line argument -- the integer for which to get the smallest factor")
+        n = int(sys.argv[1])
+    if n < 1:
+        sys.exit(sys.argv[0] + "Expecting a positive integer")
 
-smallest_prime_factor = None
-for i in range(2, n):
-    if (n % i) == 0:
-        smallest_prime_factor = i
+        smallest_prime_factor = None
+    for i in range(2, n):
+
+        if (n % i) == 0:
+            smallest_prime_factor = i
         break
 
-if smallest_prime_factor is None:
-    print(n)
-else:
-    print(smallest_prime_factor)
+    if smallest_prime_factor is None:
+        print(n)
+    else:
+        print(smallest_prime_factor)
+        return smallest_prime_factor
+
