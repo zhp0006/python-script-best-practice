83b7f6259965f24154de4166eba159eb6307bc5e
Zahida Hassan Pervaiz <69922404+zhp0006@users.noreply.github.com>
2021-03-24 23:50:35 -0500

    modified code using best practice

--- smallest_factor.py
+++ smallest_factor.py
@@ -1,20 +1,26 @@
 #! /usr/bin/env python3
+
 # A script for getting the smallest prime factor of an integer.
 
 import sys
+
 
 def get_smallest_prime_factor(n):
     for i in range(2, n):
         if (n % i) == 0:
             return i
-        return None
-if len(sys.argv) != 2:
-     sys.exit(sys.argv[0] + ": Expecting one command line argument -- the integer for which to get the smallest factor")
-n = int(sys.argv[1])
-if n < 1:
-    sys.exit(sys.argv[0] + ": Expecting a positive integer")
-smallest_prime_factor = get_smallest_prime_factor(n)
-if smallest_prime_factor is None:
+    return None
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        sys.exit(sys.argv[0] + ": Expecting one command line argument -- the integer for which to get the smallest factor")
+    n = int(sys.argv[1])
+    if n < 1:
+        sys.exit(sys.argv[0] + ": Expecting a positive integer")
+
+    smallest_prime_factor = get_smallest_prime_factor(n)
+
+    if smallest_prime_factor is None:
         print(n)
-else:
+    else:
         print(smallest_prime_factor)
